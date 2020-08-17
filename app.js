95fbf4a1a7eda6af15c5ecb1cd241de6b6eb58a6
Andy Taylor <vrdeveloperuk@gmail.com>
2020-08-17 16:09:25 +0100

    Fully set up

---
+++ app.js
@@ -0,0 +1,38 @@
+"use strict";
+    // Create Dino Constructor
+
+
+    // Create Dino Objects
+    fetch("./dino.json")
+      .then(function(resp) {
+        return resp.json();
+    })
+    .then(function(data) {
+      console.log(data);
+    });
+
+    // Create Human Object
+
+    // Use IIFE to get human data from form
+
+
+    // Create Dino Compare Method 1
+    // NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+    // Create Dino Compare Method 2
+    // NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+    // Create Dino Compare Method 3
+    // NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+    // Generate Tiles for each Dino in Array
+
+        // Add tiles to DOM
+
+    // Remove form from screen
+
+
+// On button click, prepare and display infographic
