3d598cd3f7225ddbaf91a7db2efe4ad2167b18f2
Andy Taylor <vrdeveloperuk@gmail.com>
2020-08-18 18:27:44 +0100

    Dino and human objects created

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-"use strict";
-    // Create Dino Constructor
 
+    // Create Dino Constructor
+    function Dino(species, image, fact) {
+      this.species = species;
+      this.image = image;
+      this.fact = fact;
+    }
 
     // Create Dino Objects
-    fetch("./dino.json")
-      .then(function(resp) {
-        return resp.json();
-    })
-    .then(function(data) {
-      console.log(data);
-    });
+    let dinos = [];
+    function createDinos() {
+      return fetch("./dino.json")
+        .then(function(resp) {
+          return resp.json();
+        })
+        .then(function(data) {
+          return dinos = data.Dinos.map(function (i) {
+            return new Dino(i.species, `./images/${i.species.toLowerCase()}.png`, i.fact)
+          });
+        })
+    }
 
     // Create Human Object
+    let human = {
+      Name: 'na',
+      Height: {
+        Feet: 0,
+        inches: 0
+      },
+      Weight: 0,
+      Diet: 'na'
+    };
 
     // Use IIFE to get human data from form
 
